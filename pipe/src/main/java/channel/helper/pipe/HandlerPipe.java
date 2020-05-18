f7729ac594b820c7ba6460af93a1ac51e6ef9c56
jrfeng <jrfeng331@qq.com>
2020-05-18 11:03:36 +0800

    add class: 'HandlerPipe'

---
+++ pipe/src/main/java/channel/helper/pipe/HandlerPipe.java
@@ -0,0 +1,61 @@
+package channel.helper.pipe;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+
+import channel.helper.Dispatcher;
+import channel.helper.Pipe;
+
+public class HandlerPipe extends Handler implements Pipe<Message> {
+    private static final String TAG = "HandlerPipe";
+    private final WeakReference<Dispatcher> mDispatcherWeakReference;
+
+    public HandlerPipe(Dispatcher dispatcher) {
+        mDispatcherWeakReference = new WeakReference<>(dispatcher);
+    }
+
+    public HandlerPipe(Looper looper, Dispatcher dispatcher) {
+        super(looper);
+        mDispatcherWeakReference = new WeakReference<>(dispatcher);
+    }
+
+    @Override
+    public void emitData(Map<String, Object> data) {
+        Message message = Message.obtain();
+        message.obj = data;
+        sendMessage(message);
+    }
+
+    @Override
+    public void handleMessage(@NonNull Message msg) {
+        Dispatcher dispatcher = mDispatcherWeakReference.get();
+        if (dispatcher == null) {
+            return;
+        }
+
+        dispatcher.dispatch(getData(msg));
+    }
+
+    @Override
+    public Map<String, Object> getData(Message dataWrapper) {
+        if (dataWrapper.obj == null) {
+            Log.d(TAG, "dataWrapper is empty.");
+            return new HashMap<>();
+        }
+
+        if (!(dataWrapper.obj instanceof Map)) {
+            Log.d(TAG, "dataWrapper is empty.");
+            return new HashMap<>();
+        }
+
+        return (Map<String, Object>) dataWrapper.obj;
+    }
+}
